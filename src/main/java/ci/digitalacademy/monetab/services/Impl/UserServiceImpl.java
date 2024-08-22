bb0bc9ebbd56e804565e098af4ab0699a078e780
zezeteur <hervedibi.hd@gmail.com>
2024-08-22 10:51:18 +0000

    Mise a jour v1.0.5 -Connexion base de donnees -Optimisation du code -Correction de bugs Mon Etab v1.0.5

--- src/main/java/ci/digitalacademy/monetab/services/Impl/UserServiceImpl.java
+++ src/main/java/ci/digitalacademy/monetab/services/Impl/UserServiceImpl.java
@@ -1,17 +1,23 @@
 package ci.digitalacademy.monetab.services.Impl;
 
 import ci.digitalacademy.monetab.models.User;
+import ci.digitalacademy.monetab.repositories.UserRepository;
 import ci.digitalacademy.monetab.services.UserService;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
 
+    private final UserRepository userRepository;
+
     @Override
     public User save(User user) {
-        return null;
+
+        return userRepository.save(user);
     }
 
     @Override
